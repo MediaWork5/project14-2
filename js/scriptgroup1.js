9eafbf60250c6dec4a83603d37a5ae89445b1783
Muhammad Mostafa Ahmed <39030106+MuhammadMustafa37050@users.noreply.github.com>
2020-03-06 19:13:54 +0200

    first commit

---
+++ js/scriptgroup1.js
@@ -0,0 +1,30 @@
+/*glopal $, alert, console*/
+
+$(function(){
+
+    'use strict';
+     
+    
+    $('.up .link-1').click(function(){
+        
+        $(".paragraph-1").fadeToggle(600);
+        
+    });
+    $('.up .link-2').click(function(){
+        
+        $(".paragraph-2").fadeToggle(600);
+        
+    });
+    $('.up .link-3').click(function(){
+        
+        $(".paragraph-3").fadeToggle(600);
+        
+    });
+    $('.up .link-4').click(function(){
+        
+        $(".paragraph-4").fadeToggle(600);
+        
+    });
+    
+    
+});
